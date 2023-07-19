a7cd8f62c827b6c4e39616373b2f8601811ca1ab
David <davidsati1212@gmail.com>
2023-07-18 23:43:38 -0500

    First Push

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,60 @@
+﻿using EmailMS.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+
+namespace EmailMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public EmailController(IConfiguration  config)
+        {
+            _config = config;
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> SendEmail(Message message)
+        {
+            String SendMailFrom = _config.GetSection("EmailUsername").Value;
+            String SendMailTo = message.To;
+            String SendMailSubject = message.Subject;
+            String SendMailBody = message.Body;
+
+            try
+            {
+                SmtpClient SmtpServer = new SmtpClient(_config.GetSection("EmailHost").Value, 587);
+                SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+                MailMessage email = new MailMessage();
+                // START
+                email.From = new MailAddress(SendMailFrom);
+                email.To.Add(SendMailTo);
+                email.CC.Add(SendMailFrom);
+                email.Subject = SendMailSubject;
+                email.Body = SendMailBody;
+                email.IsBodyHtml = true;
+                if (message.Priority == true) { email.Priority = MailPriority.High; }
+
+                //END
+                SmtpServer.Timeout = 5000;
+                SmtpServer.EnableSsl = true;
+                SmtpServer.UseDefaultCredentials = false;
+                SmtpServer.Credentials = new NetworkCredential(SendMailFrom, _config.GetSection("EmailPassword").Value);
+                SmtpServer.Send(email);
+
+                return Ok("Email Successfully Sent");
+                
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
+        }
+    }
+}
---
+++ Models/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace EmailMS.Models
+{
+    public class Message
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool? Priority { get; set; }
+    }
+}
